641385a3e36c5c81d29a6e71ed74a7dbcdfc91ae
lilblizzard <lilblizzard97@gmail.com>
2020-03-23 14:42:33 -0400

    quires are automatically created as children of the manuscript, quire count is displaying properly

--- app/models/manuscript.rb
+++ app/models/manuscript.rb
@@ -1,4 +1,20 @@
 class Manuscript < ApplicationRecord
   belongs_to :account
   has_many :quires, dependent: :destroy
+
+  attr_accessor :quire_count
+
+  after_save :create_quires
+
+  validates_presence_of :name, :shelfmark
+
+  def create_quires
+    if quire_count.present?
+      quire_count.to_i.times do
+        quires.create
+      end
+    end
+      # self.quire_count = nil
+  end
+
 end
