0b35441c91fcc0f9b9650eeb24cbe250c800d25f
lilblizzard <lilblizzard97@gmail.com>
2020-03-30 15:02:53 -0400

    added opposite:integer attribute to leaf and migrated db

--- app/models/quire.rb
+++ app/models/quire.rb
@@ -6,13 +6,13 @@ class Quire < ApplicationRecord
 
   # what about when quires are auto generated in manuscripts?
   after_save :create_leaves
-  # after_save :assign_leaf_positions
+  after_save :assign_leaf_positions
 
   validates_presence_of :leaf_count
 
   def create_leaves
     if leaf_count.present?
-      leaf_count.to_i.times do
+      (0..leaf_count.to_i).each do |leaf|
         leaves.create
       end
     end
@@ -21,6 +21,8 @@ class Quire < ApplicationRecord
   def assign_leaf_positions
     leaves.each_with_index do |leaf, index|
       leaf.position = index + 1
+      leaf.opposite = leaves.size - index
+      binding.pry
     end
   end
 end
